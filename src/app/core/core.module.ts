b0a22c3b220bbeecb7944593e26daf6219d95770
siyuan cao <syuancao@outlook.com>
2022-12-24 23:20:13 +0800

    use md-icon

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,19 @@ import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   exports: [HeaderComponent, FooterComponent, SidebarComponent],
-  imports: [CommonModule, BrowserAnimationsModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parent: CoreModule) {
