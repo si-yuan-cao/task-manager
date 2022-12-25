4fecfaa592ca45749e9fa30b3227217cc3209be8
siyuan cao <syuancao@outlook.com>
2022-12-25 23:06:06 +0800

    Add some project related components

---
+++ src/app/project/project.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectItemComponent } from './project-item/project-item.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { InviteComponent } from './invite/invite.component';
+
+@NgModule({
+  declarations: [
+    ProjectListComponent,
+    ProjectItemComponent,
+    NewProjectComponent,
+    InviteComponent,
+  ],
+  imports: [CommonModule],
+})
+export class ProjectModule {}
